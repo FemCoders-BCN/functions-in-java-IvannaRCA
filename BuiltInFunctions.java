163c98c2022a13dec7e9349e39cfbc321cb6cd70
Ivanna Caraccio <ivacaraccio@hotmail.com>
2026-06-02 15:01:46 +0200

    feat: finish BuiltInFunctions exercise

--- BuiltInFunctions.java
+++ BuiltInFunctions.java
@@ -1,8 +1,15 @@
 public class BuiltInFunctions {
     public static void main(String[] args) {
-
         //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
-
+        printMessage("Hola, me llamo Ivanna");
+        System.out.println(getLength("cuánto mide este mensaje"));
+        System.out.println(convertToLowercase("FeMcOdErS"));
+        System.out.println(findPrefix("Hola", "H"));
+        System.out.println(replaceCharacters("Banana", 'n', 't'));
+        System.out.println(getsSquareRoot(4));
+        System.out.println(getPower(2, 8));
+        System.out.println(getRandomNumber());
+        System.out.println(maxNumber(5, 10));
     }
 
         //Ejercicio 1:
@@ -14,8 +21,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a java built-in function that prints the message on the console.
         */
-
-        // Escribe tu código aquí
+        public static void printMessage(String message) {
+            System.out.println(message);
+        }
         
     
         //Ejercicio 2:
@@ -28,8 +36,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that returns the length of a string.
          */
-
-        // Escribe tu código aquí
+         public static int getLength(String message) {
+            return message.length();
+         }
 
 
         //Ejercicio 3:
@@ -42,8 +51,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that converts a string to lowercase.
          */
-
-        // Escribe tu código aquí
+         public static String convertToLowercase(String message) {
+            return message.toLowerCase();
+         }
 
 
         //Ejercicio 4:
@@ -57,9 +67,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that checks if a string starts with a specified prefix.
          */
-
-        // Escribe tu código aquí
-
+         public static boolean findPrefix(String message, String prefix) {
+            return message.startsWith(prefix);
+         }
 
         //Ejercicio 5:
         /**
@@ -73,8 +83,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that replaces all occurrences of a specified character in a string with another character.
          */
-
-        // Escribe tu código aquí
+         public static String replaceCharacters(String message, char oldChar, char newChar) {
+            return message.replace(oldChar, newChar);
+         }
 
 
         //Ejercicio 6:
@@ -87,9 +98,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that calculates the square root of a number.
          */
-
-        // Escribe tu código aquí
-
+         public static double getsSquareRoot(double number) {
+            return Math.sqrt(number);
+         }
 
         //Ejercicio 7:
         /**
@@ -102,9 +113,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that calculates the power of a number.
          */
-
-        // Escribe tu código aquí
-
+         public static double getPower(double base, double exponent) {
+            return Math.pow(base, exponent);
+         }
 
         //Ejercicio 8:
         /**
@@ -115,9 +126,9 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that generates a random number between 0.0 (inclusive) and 1.0 (exclusive).
          */
-
-        // Escribe tu código aquí
-
+         public static double getRandomNumber() {
+            return Math.random();
+         }
 
         //Ejercicio 9:
         /**
@@ -130,6 +141,7 @@ public class BuiltInFunctions {
          * Inside the function:
          * 1. Find a built-in function that returns the larger of two numbers.
          */
-
-        // Escribe tu código aquí
+         public static int maxNumber(int number1, int number2) {
+            return Math.max(number1, number2);
+         }
     }
